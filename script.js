256f0723212e75feeab639ccdc91bf569941b28b
ferdyansyah <99112318+Ferdiansahh@users.noreply.github.com>
2022-05-29 10:22:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let btc = document.getElementById("bitcoin");
+let eth = document.getElementById("ethereum");
+let dogecoin = document.getElementById("dogecoin");
+
+let settings = {
+  async: true,
+  scrossDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+  method: "GET",
+  headers: {},
+};
+
+$.ajax(settings).done(function (response) {
+  btc.innerHTML = response.bitcoin.usd;
+  eth.innerHTML = response.ethereum.usd;
+  dogecoin.innerHTML = response.dogecoin.usd;
+});
